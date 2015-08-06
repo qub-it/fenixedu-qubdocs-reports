458523fdbc9a2a58dbc14db04229e38108e9a6ea
Diogo Simoes <diogo.simoes@qub-it.com>
2015-08-06 20:37:49 +0100

    Updated configuration script for RUL.

--- src/main/java/org/fenixedu/qubdocs/task/RULConfigurationScript.java
+++ src/main/java/org/fenixedu/qubdocs/task/RULConfigurationScript.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Map.Entry;
+
 import org.fenixedu.academic.domain.administrativeOffice.AdministrativeOffice;
 import org.fenixedu.academic.domain.person.Gender;
 import org.fenixedu.academic.domain.serviceRequests.ServiceRequestType;
@@ -20,6 +21,7 @@ public class RULConfigurationScript extends CustomTask {
 
     @Override
     public void runTask() throws Exception {
+        /* DocumentPurposeTypeInstances */
         Map<String, LocalizedString> documentPurposeTypes = new HashMap<String, LocalizedString>();
         documentPurposeTypes.put("FAMILY_ALLOWANCE", new LocalizedString(pt, "Abono de Família").with(en, "Family Allowance"));
         documentPurposeTypes.put("FAMILY_BENEFITS", new LocalizedString(pt, "Prestações Familiares").with(en, "Family Benefits"));
@@ -28,7 +30,7 @@ public class RULConfigurationScript extends CustomTask {
         documentPurposeTypes.put("SOCIAL_SECURITY", new LocalizedString(pt, "Segurança Social").with(en, "Social Security"));
         documentPurposeTypes.put("MILITARY", new LocalizedString(pt, "Fins Militares").with(en, "Military Purposes"));
         documentPurposeTypes.put("SUB23", new LocalizedString(pt, "Sub23").with(en, "Sub23"));
-        documentPurposeTypes.put("STUDY_SCHOLARSHIP", new LocalizedString(pt, "Bolsa de Estudo").with(en, "Scholarship"));
+        documentPurposeTypes.put("STUDY_SCHOLARSHIP", new LocalizedString(pt, "Boslsa de Estudo").with(en, "Scholarship"));
         documentPurposeTypes.put("PROFESSIONAL", new LocalizedString(pt, "Fins Profissionais").with(en, "Professional purposes"));
         documentPurposeTypes.put("PPRE", new LocalizedString(pt, "PPRE").with(en, "PPRE"));
         documentPurposeTypes.put("SCHOOL_INSURANCE", new LocalizedString(pt, "Seguro Escolar").with(en, "School Insurance"));
