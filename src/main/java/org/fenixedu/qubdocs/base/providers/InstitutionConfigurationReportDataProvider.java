bcc9c6fb7d170ba6235a00f5aaeac85d22dc8c2a
JNPA <joao.amaral@qub-it.com>
2017-03-10 13:07:55 +0000

    Add new variables to use in generating documents

--- src/main/java/org/fenixedu/qubdocs/base/providers/InstitutionConfigurationReportDataProvider.java
+++ src/main/java/org/fenixedu/qubdocs/base/providers/InstitutionConfigurationReportDataProvider.java
@@ -7,17 +7,27 @@ public class InstitutionConfigurationReportDataProvider implements IReportDataPr
 
     protected static final String KEY_LOGO = "institutionLogo";
     protected static final String KEY_NAME = "institutionName";
+    protected static final String KEY_SHORT_NAME = "institutionShortName";
+    protected static final String KEY_ADDRESS = "institutionAddress";
+    protected static final String KEY_SITE = "institutionSite";
 
-    private String institutionName;
-    private byte[] institutionLogo;
+    private final String institutionName;
+    private final String institutionShortName;
+    private final String institutionAddress;
+    private final String institutionSite;
+    private final byte[] institutionLogo;
 
-    public InstitutionConfigurationReportDataProvider(final String name, final byte[] image) {
+    public InstitutionConfigurationReportDataProvider(final String name, final String shortName, final String address,
+            final String site, final byte[] image) {
         institutionName = name;
+        institutionShortName = shortName;
+        institutionAddress = address;
+        institutionSite = site;
         institutionLogo = image;
     }
 
     @Override
-    public void registerFieldsAndImages(IDocumentFieldsData documentFieldsData) {
+    public void registerFieldsAndImages(final IDocumentFieldsData documentFieldsData) {
         if (institutionLogo.length == 0) {
             return;
         }
@@ -25,14 +35,20 @@ public class InstitutionConfigurationReportDataProvider implements IReportDataPr
     }
 
     @Override
-    public boolean handleKey(String key) {
-        return key.equals(KEY_NAME);
+    public boolean handleKey(final String key) {
+        return key.equals(KEY_NAME) || key.equals(KEY_SHORT_NAME) || key.equals(KEY_ADDRESS) || key.equals(KEY_SITE);
     }
 
     @Override
-    public Object valueForKey(String key) {
+    public Object valueForKey(final String key) {
         if (key.equals(KEY_NAME)) {
             return institutionName;
+        } else if (key.equals(KEY_SHORT_NAME)) {
+            return institutionShortName;
+        } else if (key.equals(KEY_SITE)) {
+            return institutionSite;
+        } else if (key.equals(KEY_ADDRESS)) {
+            return institutionAddress;
         } else {
             return null;
         }
