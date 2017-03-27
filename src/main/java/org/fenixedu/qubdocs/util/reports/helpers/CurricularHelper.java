6e5f8e68da591639085e722e3ffc9e96e5ee8293
JNPA <joao.amaral@qub-it.com>
2017-03-27 20:54:39 +0100

    Redo the algorithm to get the higher cycle giving an ICurriculumEntry

--- src/main/java/org/fenixedu/qubdocs/util/reports/helpers/CurricularHelper.java
+++ src/main/java/org/fenixedu/qubdocs/util/reports/helpers/CurricularHelper.java
@@ -1,75 +1,42 @@
 package org.fenixedu.qubdocs.util.reports.helpers;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.fenixedu.academic.domain.CurricularCourse;
-import org.fenixedu.academic.domain.Enrolment;
-import org.fenixedu.academic.domain.degreeStructure.CycleCourseGroup;
 import org.fenixedu.academic.domain.degreeStructure.CycleType;
+import org.fenixedu.academic.domain.degreeStructure.DegreeModule;
+import org.fenixedu.academic.domain.student.curriculum.ICurriculumEntry;
+import org.fenixedu.academic.domain.studentCurriculum.CurriculumLine;
+import org.fenixedu.academic.domain.studentCurriculum.CycleCurriculumGroup;
 
 import com.qubit.terra.docs.util.helpers.IDocumentHelper;
 
 public class CurricularHelper implements IDocumentHelper {
 
-    private boolean isNormalEnrolment(Enrolment enrolment) {
-        return enrolment.isAnnulled()
-                && !enrolment.isStandalone()
-                && !enrolment.isExtraCurricular()
-                && !enrolment.isPropaedeutic()
-                && (enrolment.getCurriculumGroup().isInternalCreditsSourceGroup() || !enrolment.getCurriculumGroup()
-                        .isNoCourseGroupCurriculumGroup())
-                && (enrolment.getParentCycleCurriculumGroup() == null || !enrolment.getParentCycleCurriculumGroup().isExternal());
-    }
+    public CycleType getCourseHigherCycle(final ICurriculumEntry curriculumEntry, final boolean competenceScope) {
 
-    public CycleType inferCourseHigherCycle(Enrolment enrolment, boolean competenceScope) {
-        if (isNormalEnrolment(enrolment)) {
-            return enrolment.getParentCycleCurriculumGroup().getCycleType();
+        if (!(curriculumEntry instanceof CurriculumLine)) {
+            return null;
         }
-        if (competenceScope) {
-            Set<CycleCourseGroup> sample = new HashSet<CycleCourseGroup>();
-            CycleType result = null;
-            for (CurricularCourse course : enrolment.getCurricularCourse().getCompetenceCourse()
-                    .getAssociatedCurricularCoursesSet()) {
-                sample.addAll(course.getParentCycleCourseGroups());
-            }
-            for (CycleCourseGroup group : sample) {
-                if (result == null) {
-                    result = group.getCycleType();
-                } else if (result.isBeforeOrEquals(group.getCycleType())) {
-                    result = group.getCycleType();
-                }
-            }
-            return result;
-        } else {
-            return enrolment.getCurricularCourse().getDegree().getCycleTypes().stream()
-                    .sorted(CycleType.COMPARATOR_BY_GREATER_WEIGHT).findFirst().orElse(null);
-        }
-    }
 
-    public CycleType inferCourseLowerCycle(Enrolment enrolment, boolean competenceScope) {
-        if (isNormalEnrolment(enrolment)) {
-            return enrolment.getParentCycleCurriculumGroup().getCycleType();
+        final CurriculumLine curriculumLine = (CurriculumLine) curriculumEntry;
+
+        if (!competenceScope) {
+            final CycleCurriculumGroup cycleCurriculumGroup = curriculumLine.getParentCycleCurriculumGroup();
+            return cycleCurriculumGroup != null ? cycleCurriculumGroup.getCycleType() : null;
         }
-        if (competenceScope) {
-            Set<CycleCourseGroup> sample = new HashSet<CycleCourseGroup>();
-            CycleType result = null;
-            for (CurricularCourse course : enrolment.getCurricularCourse().getCompetenceCourse()
-                    .getAssociatedCurricularCoursesSet()) {
-                sample.addAll(course.getParentCycleCourseGroups());
-            }
-            for (CycleCourseGroup group : sample) {
-                if (result == null) {
-                    result = group.getCycleType();
-                } else if (group.getCycleType().isBeforeOrEquals(result)) {
-                    result = group.getCycleType();
-                }
-            }
-            return result;
-        } else {
-            return enrolment.getCurricularCourse().getDegree().getCycleTypes().stream()
-                    .sorted(CycleType.COMPARATOR_BY_LESS_WEIGHT).findFirst().orElse(null);
+
+        final DegreeModule degreeModule = curriculumLine.getDegreeModule();
+        if (!(degreeModule instanceof CurricularCourse)) {
+            return null;
         }
+
+        return ((CurricularCourse) degreeModule).getCompetenceCourse().getAssociatedCurricularCoursesSet().stream()
+
+                .flatMap(cc -> cc.getParentCycleCourseGroups().stream())
+
+                .map(c -> c.getCycleType())
+
+                .sorted(CycleType.COMPARATOR_BY_GREATER_WEIGHT).findFirst().orElse(null);
+
     }
 
 }
