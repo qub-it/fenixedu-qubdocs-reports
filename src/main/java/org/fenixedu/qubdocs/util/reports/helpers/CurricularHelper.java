fd91d107ecb0a392c3cb1d9eb306080ebcf145fe
JNPA <joao.amaral@qub-it.com>
2017-05-03 11:42:20 +0100

    Minor fix in algorithm to calculate the cicle of an entry.

--- src/main/java/org/fenixedu/qubdocs/util/reports/helpers/CurricularHelper.java
+++ src/main/java/org/fenixedu/qubdocs/util/reports/helpers/CurricularHelper.java
@@ -1,5 +1,8 @@
 package org.fenixedu.qubdocs.util.reports.helpers;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.fenixedu.academic.domain.CurricularCourse;
 import org.fenixedu.academic.domain.degreeStructure.CycleType;
 import org.fenixedu.academic.domain.degreeStructure.DegreeModule;
@@ -10,6 +13,9 @@ import org.fenixedu.academic.domain.studentCurriculum.CycleCurriculumGroup;
 import com.qubit.terra.docs.util.helpers.IDocumentHelper;
 
 public class CurricularHelper implements IDocumentHelper {
+
+    private static final List<CycleType> ACCEPTED_CYCLES =
+            Arrays.asList(CycleType.FIRST_CYCLE, CycleType.SECOND_CYCLE, CycleType.THIRD_CYCLE);
 
     public CycleType getCourseHigherCycle(final ICurriculumEntry curriculumEntry, final boolean competenceScope) {
 
@@ -31,9 +37,13 @@ public class CurricularHelper implements IDocumentHelper {
 
         return ((CurricularCourse) degreeModule).getCompetenceCourse().getAssociatedCurricularCoursesSet().stream()
 
+                .filter(c -> !c.getParentContextsByExecutionYear(curriculumEntry.getExecutionYear()).isEmpty())
+
                 .flatMap(cc -> cc.getParentCycleCourseGroups().stream())
 
                 .map(c -> c.getCycleType())
+
+                .filter(c -> ACCEPTED_CYCLES.contains(c))
 
                 .sorted(CycleType.COMPARATOR_BY_GREATER_WEIGHT).findFirst().orElse(null);
 
